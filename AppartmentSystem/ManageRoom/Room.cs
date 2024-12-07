f64da65446fedce8f7887dd2009b3a4d3f1692db
Nats <burgostristan96@gmail.com>
2024-12-08 00:56:44 +0800

    update

--- AppartmentSystem/ManageRoom/Room.cs
+++ AppartmentSystem/ManageRoom/Room.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Configuration;
 using System.Data;
+using System.Data.Common;
 using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
@@ -25,9 +26,40 @@ namespace AppartmentSystem
         {
 
         }
-
+        //pwede pang mabago
         private void frm_room_Load(object sender, EventArgs e)
         {
+            string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            roomAddingDAL add = new roomAddingDAL(connectionString);
+
+            string query = @"
+                SELECT
+                     r.room_id,
+                     t.tenant_name,
+                     t.moved_in
+                FROM room r
+                LEFT JOIN tenant t
+                ON r.room_id = t.room_id";
+
+            using(SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+
+                    dg_ManageRoom.DataSource = dataTable;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+                     
 
         }
 
@@ -38,7 +70,7 @@ namespace AppartmentSystem
 
         private void btn_addRoom_Click(object sender, EventArgs e)
         {
-
+            //Connection ng database
             string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
             roomAddingDAL add = new roomAddingDAL(connectionString);
 
@@ -50,6 +82,7 @@ namespace AppartmentSystem
             //mali yung design
             DateTime moved_In = DateTime.Now;
 
+            //eto yung kinuha yung process
             bool success = add.AddRoomAndTenant(roomNum, tenantName, moved_In, roomPrice);
 
             if(success)
